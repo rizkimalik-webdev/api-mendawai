614c52cae5267e15192a294b0ab76ee4490eb57e
Riski Malik <rizkimalik94@outlook.com>
2021-12-03 14:16:23 +0700

    add modul service

---
+++ svc-install.js
@@ -0,0 +1,16 @@
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+    name:'api_mendawai',
+    description: 'Api web server.',
+    script: 'C:\\Sosmed\\ApiMendawai\\app.js'
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+    svc.start();
+});
+
+svc.install();
