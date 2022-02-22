3878f7692c9cecdd3e21f26900655468a35c9edf
Riski Malik <rizkimalik94@outlook.com>
2022-02-23 00:03:18 +0700

    modif seed

--- seeds/user_level.js
+++ seeds/user_level.js
@@ -3,9 +3,9 @@ exports.seed = function (knex) {
     return knex('user_level').truncate()
         .then(function () {
             return knex('user_level').insert([
-                { level_name: 'Layer1', description: 'Level L1 CS Agents' },
-                { level_name: 'Layer2', description: 'Level CS Team Leaders / Case Unit' },
-                { level_name: 'Layer3', description: 'Level Layer 3 / PIC' },
+                { level_name: 'Layer1', description: 'Level CS Agents' },
+                { level_name: 'Layer2', description: 'Level CS Team Leaders' },
+                { level_name: 'Layer3', description: 'Level Case Unit / PIC' },
                 { level_name: 'Supervisor', description: 'Level Supervisor' },
                 { level_name: 'Administrator', description: 'Level Administrator' },
             ]);
