f0a38b2b95aa2e458f989b532e2cb4654b961907
Riski Malik <rizkimalik94@outlook.com>
2022-02-26 15:24:50 +0700

    add view_chats

---
+++ migrations/view_chats.js
@@ -0,0 +1,14 @@
+
+const up = function (knex) {
+    return knex.raw(`CREATE VIEW view_chats AS (
+        SELECT * FROM chats
+        union
+        select * from chats_end
+    )`);
+};
+
+const down = function (knex) {
+    return knex.raw(`DROP VIEW view_chats`);
+};
+
+module.exports = { up, down }
