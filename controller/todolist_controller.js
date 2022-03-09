252bc47e14cbb85ae9f91aa30090001949e956c2
Riski Malik <rizkimalik94@outlook.com>
2022-03-09 17:57:50 +0700

    update todolist

--- controller/todolist_controller.js
+++ controller/todolist_controller.js
@@ -1,5 +1,6 @@
 'use strict';
 const knex = require('../config/db_connect');
+const date = require('date-and-time');
 const { auth_jwt_bearer } = require('../middleware');
 const logger = require('../helper/logger');
 const response = require('../helper/json_response');
@@ -12,17 +13,17 @@ const total_ticket = async function (req, res) {
 
         let tickets;
         if (user_level === 'Layer1') {
-            tickets = await knex.raw(`SELECT status, (select count(ticket_number) FROM tickets where status=status.status AND org_id='${organization_id}' AND user_create='${user_create}') as total FROM status`);
+            tickets = await knex.raw(`SELECT status, (select count(ticket_number) FROM tickets where status=status.status AND org_id='${organization_id}' AND user_create='${user_create}') as total FROM status ORDER BY id ASC`);
         }
         else if (user_level === 'Layer2') {
-            tickets = await knex.raw(`SELECT status, (select count(ticket_number) FROM tickets where status=status.status AND org_id='${organization_id}' AND ticket_position='2') as total FROM status`);
+            tickets = await knex.raw(`SELECT status, (select count(ticket_number) FROM tickets where status=status.status AND org_id='${organization_id}' AND ticket_position='2') as total FROM status ORDER BY id ASC`);
         }
         else if (user_level === 'Layer3') {
-            tickets = await knex.raw(`SELECT status, (select count(ticket_number) FROM tickets where status=status.status AND department_id='${department_id}' AND ticket_position='3') as total FROM status`);
+            tickets = await knex.raw(`SELECT status, (select count(ticket_number) FROM tickets where status=status.status AND department_id='${department_id}' AND ticket_position='3') as total FROM status ORDER BY id ASC`);
         }
         else {
             //? user_level = admin
-            tickets = await knex.raw(`SELECT status, (select count(ticket_number) FROM tickets where status=status.status AND org_id='${organization_id}') as total FROM status`);
+            tickets = await knex.raw(`SELECT status, (select count(ticket_number) FROM tickets where status=status.status AND org_id='${organization_id}') as total FROM status ORDER BY id ASC`);
         }
 
         response.ok(res, tickets);
@@ -53,6 +54,10 @@ const data_ticket = async function (req, res) {
         else {
             //? user_level = admin
             tickets = await knex.raw(`SELECT * FROM view_tickets WHERE status='${status}' AND org_id='${organization_id}'`);
+        }
+
+        for (let i = 0; i < tickets.length; i++) {
+            tickets[i].date_create = date.format(tickets[i].date_create, 'YYYY-MM-DD HH:mm:SS', true)
         }
 
         response.ok(res, tickets);
